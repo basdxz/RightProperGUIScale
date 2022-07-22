8d06b9dc4406c946510ab707844523fea4a1862e
basdxz <tudurap.com@gmail.com>
2022-07-22 23:32:51 +0100

    Move server side crash

--- src/main/java/com/github/basdxz/rightproperguiscale/CommonProxy.java
+++ src/main/java/com/github/basdxz/rightproperguiscale/CommonProxy.java
@@ -3,10 +3,7 @@ package com.github.basdxz.rightproperguiscale;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 public class CommonProxy {
-    public CommonProxy() {
-        throw new RuntimeException("Laughing at this user! They got a client side mod in their server instance!");
-    }
-
     public void preInit(FMLPreInitializationEvent event) {
+        throw new RuntimeException("Laughing at this user! They got a client side mod in their server instance!");
     }
 }
