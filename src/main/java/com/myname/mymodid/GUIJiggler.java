3f9cc1c58de02f72c1a695a33348e69f233e7128
basdxz <tudurap.com@gmail.com>
2022-07-22 15:01:45 +0100

    Work towards the fancy slider

--- src/main/java/com/myname/mymodid/GUIJiggler.java
+++ src/main/java/com/myname/mymodid/GUIJiggler.java
@@ -8,7 +8,16 @@ import org.apache.commons.lang3.reflect.FieldUtils;
 
 @UtilityClass
 public final class GUIJiggler {
-    private static final String[] OPTIONS_ENUM_FLOAT_BOOLEAN_FIELD_NAMES = new String[]{"enumFloat", "bbm"};
+    //TODO: better naming, this is not readable
+    private static final String[] OPTIONS_ENUM_FLOAT_FIELD_NAMES = new String[]{"enumFloat", "field_74385_A"};
+    private static final boolean OPTIONS_ENUM_FLOAT_FIELD_GUI_SCALE_DEFAULT = false;
+    private static final String[] OPTIONS_VALUE_MIN_FIELD_NAMES = new String[]{"valueMin", "field_148271_N"};
+    private static final float OPTIONS_ENUM_FLOAT_VALUE_STEP_GUI_SCALE_DEFAULT = 0F;
+    private static final String[] OPTIONS_VALUE_MAX_FIELD_NAMES = new String[]{"valueMax", "field_148272_O"};
+    private static final float OPTIONS_ENUM_FLOAT_VALUE_MIN_GUI_SCALE_DEFAULT = 0F;
+    private static final String[] OPTIONS_VALUE_STEP_FIELD_NAMES = new String[]{"valueStep", "field_148270_M"};
+    private static final float OPTIONS_ENUM_FLOAT_VALUE_MAX_GUI_SCALE_DEFAULT = 0F;
+
 
     public static void init() {
         guiScaleButtonToSlider();
@@ -18,7 +27,19 @@ public final class GUIJiggler {
         setField(GameSettings.Options.class,
                  GameSettings.Options.GUI_SCALE,
                  true,
-                 OPTIONS_ENUM_FLOAT_BOOLEAN_FIELD_NAMES);
+                 OPTIONS_ENUM_FLOAT_FIELD_NAMES);
+        setField(GameSettings.Options.class,
+                 GameSettings.Options.GUI_SCALE,
+                 0F,
+                 OPTIONS_VALUE_MIN_FIELD_NAMES);
+        setField(GameSettings.Options.class,
+                 GameSettings.Options.GUI_SCALE,
+                 10F,
+                 OPTIONS_VALUE_MAX_FIELD_NAMES);
+        setField(GameSettings.Options.class,
+                 GameSettings.Options.GUI_SCALE,
+                 1F,
+                 OPTIONS_VALUE_STEP_FIELD_NAMES);
     }
 
     @SneakyThrows
@@ -28,6 +49,6 @@ public final class GUIJiggler {
                                      @NonNull String... fieldNameAliases) {
         val field = ReflectionHelper.findField(clazz, fieldNameAliases);
         FieldUtils.removeFinalModifier(field);
-        field.set(target, value);
+        FieldUtils.writeField(field, target, value);
     }
 }
