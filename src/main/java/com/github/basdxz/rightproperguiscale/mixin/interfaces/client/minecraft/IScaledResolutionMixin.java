fb3dd2b6438a2107a5bbb36cbf7b61a339727959
basdxz <tudurap.com@gmail.com>
2022-07-27 00:12:42 +0100

    Document Scaled Resolution

--- src/main/java/com/github/basdxz/rightproperguiscale/mixin/interfaces/client/minecraft/IScaledResolutionMixin.java
+++ src/main/java/com/github/basdxz/rightproperguiscale/mixin/interfaces/client/minecraft/IScaledResolutionMixin.java
@@ -1,15 +1,61 @@
 package com.github.basdxz.rightproperguiscale.mixin.interfaces.client.minecraft;
 
+import com.github.basdxz.rightproperguiscale.mixin.mixins.client.minecraft.ScaledResolutionMixin;
+import net.minecraft.client.gui.ScaledResolution;
+
+/**
+ * An interface applied to {@link ScaledResolution} by {@link ScaledResolutionMixin} to provide more expanded access.
+ */
 public interface IScaledResolutionMixin {
+    /**
+     * Provides the scaled down width as an int.
+     * <p>
+     * Equivalent to using: {@link ScaledResolution#getScaledWidth()}.
+     *
+     * @return scaled width as an int
+     */
     int scaledWidth();
 
+    /**
+     * Provides the scaled down height as an int.
+     * <p>
+     * Equivalent to using: {@link ScaledResolution#getScaledHeight()}.
+     *
+     * @return scaled height as an int
+     */
     int scaledHeight();
 
+    /**
+     * Provides the scaled down width as a double.
+     * <p>
+     * Equivalent to using: {@link ScaledResolution#getScaledWidth_double()}.
+     *
+     * @return scaled width as a double
+     */
     double scaledWidthD();
 
+    /**
+     * Provides the scaled down height as a double.
+     * <p>
+     * Equivalent to using: {@link ScaledResolution#getScaledHeight_double()}.
+     *
+     * @return scaled height as a double
+     */
     double scaledHeightD();
 
+    /**
+     * Provides the scale factor as an int.
+     * <p>
+     * Equivalent to using: {@link ScaledResolution#getScaleFactor()}.
+     *
+     * @return scale factor as an int
+     */
     int scaleFactor();
 
+    /**
+     * Provides the scale factor.
+     *
+     * @return scale factor as float
+     */
     float scaleFactorF();
 }
--- src/main/java/com/github/basdxz/rightproperguiscale/mixin/mixins/client/minecraft/ScaledResolutionMixin.java
+++ src/main/java/com/github/basdxz/rightproperguiscale/mixin/mixins/client/minecraft/ScaledResolutionMixin.java
@@ -14,6 +14,11 @@ import org.spongepowered.asm.mixin.injection.callback.*;
 import static com.github.basdxz.rightproperguiscale.config.RightProperGUIScaleConfig.MIN_SCALED_HEIGHT;
 import static com.github.basdxz.rightproperguiscale.config.RightProperGUIScaleConfig.MIN_SCALED_WIDTH;
 
+/**
+ * Mixin for {@link ScaledResolution} to replace how it's calculated. This implementation modifies it to determine it
+ * arithmetically rather than iteratively like in the standard implementation. Providing the benefit of fractional
+ * scales but with the downside of causing non-exact integer scaling.
+ */
 @Getter
 @Mixin(ScaledResolution.class)
 @Accessors(fluent = true, chain = true)
@@ -28,62 +33,135 @@ public abstract class ScaledResolutionMixin implements IScaledResolutionMixin {
     private double scaledHeightD;
     @Shadow
     private int scaleFactor;
-
+    /**
+     * The new scale factor float, more precise than the old {@link ScaledResolution#scaleFactor}.
+     */
+    @Unique
     private float scaleFactorF;
 
+    /**
+     * Injects at the return of {@link ScaledResolution#ScaledResolution}, differing the actual logic to this point.
+     *
+     * @param minecraft minecraft
+     * @param width     screen width
+     * @param height    screen height
+     * @param ci        mixin callback info
+     */
     @Inject(method = "<init>(Lnet/minecraft/client/Minecraft;II)V",
             at = @At(value = "RETURN"),
             require = 1)
     private void deferredConstructor(Minecraft minecraft, int width, int height, CallbackInfo ci) {
-        initScaleFactorFloat(width, height);
-        initScaledWidth(width);
-        initScaledHeight(height);
+        initScaleFactorF(width, height);
         initScaleFactor();
+        initScaledWidthD(width);
+        initScaledWidth();
+        initScaledHeightD(height);
+        initScaledHeight();
         updateLastScaledResolution();
     }
 
+    /**
+     * Initializes {@link ScaledResolutionMixin#scaleFactorF}.
+     *
+     * @param width  screen width
+     * @param height screen height
+     */
     @Unique
-    private void initScaleFactorFloat(int width, int height) {
+    private void initScaleFactorF(int width, int height) {
         scaleFactorF = maxScaleFactor(width, height);
     }
 
+    /**
+     * Provides the maximum scale factor.
+     *
+     * @param width  screen width
+     * @param height screen height
+     * @return maximum scale factor
+     */
     @Unique
     private float maxScaleFactor(int width, int height) {
         return Math.min(GUIScale.asFloat(), maxScaleFactorFromResolution(width, height));
     }
 
+    /**
+     * Provides the maximum scale factor with respect to resolution.
+     *
+     * @param width  screen width
+     * @param height screen height
+     * @return maximum scale factor
+     */
     @Unique
     private float maxScaleFactorFromResolution(int width, int height) {
         return Math.min(maxScaleFactorFromWidth(width), maxScaleFactorFromHeight(height));
     }
 
+    /**
+     * Provides the maximum scale factor with respect to horizontal resolution.
+     *
+     * @param width screen width
+     * @return maximum scale factor
+     */
     @Unique
     private float maxScaleFactorFromWidth(int width) {
         return Math.max((float) width / MIN_SCALED_WIDTH, 1F);
     }
 
+    /**
+     * Provides the maximum scale factor with respect to vertical resolution.
+     *
+     * @param height screen height
+     * @return maximum scale factor
+     */
     @Unique
     private float maxScaleFactorFromHeight(int height) {
         return Math.max((float) height / MIN_SCALED_HEIGHT, 1F);
     }
 
-    @Unique
-    private void initScaledWidth(int width) {
-        scaledWidthD = width / scaleFactorF;
-        scaledWidth = MathHelper.ceiling_double_int(scaledWidthD);
-    }
-
-    @Unique
-    private void initScaledHeight(int height) {
-        scaledHeightD = height / scaleFactorF;
-        scaledHeight = MathHelper.ceiling_double_int(scaledHeightD);
-    }
-
+    /**
+     * Initializes {@link ScaledResolution#scaleFactor}.
+     */
     @Unique
     private void initScaleFactor() {
         scaleFactor = Math.max(Math.round(scaleFactorF), 1);
     }
 
+    /**
+     * Initializes {@link ScaledResolution#scaledWidthD}.
+     *
+     * @param width screen width
+     */
+    @Unique
+    private void initScaledWidthD(int width) {
+        scaledWidthD = width / scaleFactorF;
+    }
+
+    /**
+     * Initializes {@link ScaledResolution#scaledWidth}.
+     */
+    @Unique
+    private void initScaledWidth() {
+        scaledWidth = MathHelper.ceiling_double_int(scaledWidthD);
+    }
+
+    /**
+     * Initializes {@link ScaledResolution#scaledHeightD}.
+     */
+    @Unique
+    private void initScaledHeightD(int height) {
+        scaledHeightD = height / scaleFactorF;
+    }
+
+    /**
+     * Initializes {@link ScaledResolution#scaledHeight}.
+     */
+    @Unique
+    private void initScaledHeight() {
+        scaledHeight = MathHelper.ceiling_double_int(scaledHeightD);
+    }
+
+    /**
+     * Updates last scaled resolution in {@link GUIScale}.
+     */
     @Unique
     private void updateLastScaledResolution() {
         GUIScale.lastScaledResolution(this);
