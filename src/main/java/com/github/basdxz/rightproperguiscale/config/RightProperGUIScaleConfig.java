6733a2c2ed9fa53ae6208a3bbdc95aff3c0e8dfe
basdxz <tudurap.com@gmail.com>
2022-07-26 03:33:41 +0100

    Added config text

--- src/main/java/com/github/basdxz/rightproperguiscale/config/RightProperGUIScaleConfig.java
+++ src/main/java/com/github/basdxz/rightproperguiscale/config/RightProperGUIScaleConfig.java
@@ -10,32 +10,32 @@ import lombok.experimental.*;
 @UtilityClass
 @Config(modid = Tags.MODID)
 public final class RightProperGUIScaleConfig {
-    @Config.Comment("Foo")
+    @Config.Comment("Minimum setting of the GUI Scale.")
     @Config.LangKey("config.rightproperguiscale.min")
     @Config.DefaultFloat(1F)
     @Config.RangeFloat(min = 0.1F, max = 1000F)
     public static float GUI_SCALE_MIN;
-    @Config.Comment("Foo")
+    @Config.Comment("Maxiumum setting of the GUI Scale.")
     @Config.LangKey("config.rightproperguiscale.max")
     @Config.DefaultFloat(10F)
     @Config.RangeFloat(min = 1F, max = 1000F)
     public static float GUI_SCALE_MAX = 10F;
-    @Config.Comment("Foo")
+    @Config.Comment("The step size of the GUI Scale slider.")
     @Config.LangKey("config.rightproperguiscale.step")
     @Config.DefaultFloat(0.1F)
     @Config.RangeFloat(min = 0.01F, max = 1000F)
     public static float GUI_SCALE_STEP = 0.1F;
-    @Config.Comment("Foo")
+    @Config.Comment("The Default GUI Scale.")
     @Config.LangKey("config.rightproperguiscale.default")
     @Config.DefaultFloat(4F)
     @Config.RangeFloat(min = 0.01F, max = 1000F)
     public static float GUI_SCALE_DEFAULT;
-    @Config.Comment("Foo")
+    @Config.Comment("The minimum width the GUI will scale to.")
     @Config.LangKey("config.rightproperguiscale.min_scaled_width")
     @Config.DefaultInt(320)
     @Config.RangeInt(min = 80, max = 15360)
     public static int MIN_SCALED_WIDTH;
-    @Config.Comment("Foo")
+    @Config.Comment("The minimum height the GUI will scale to.")
     @Config.LangKey("config.rightproperguiscale.min_scaled_height")
     @Config.DefaultInt(240)
     @Config.RangeInt(min = 60, max = 8640)
