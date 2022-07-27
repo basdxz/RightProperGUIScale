604d61d21dcad30e84a038c1b28ee0dfc96160c1
basdxz <tudurap.com@gmail.com>
2022-07-27 05:46:56 +0100

    Chat GUI mixin documentation and cleanup

--- src/main/java/com/github/basdxz/rightproperguiscale/mixin/mixins/client/minecraft/GuiNewChatMixin.java
+++ src/main/java/com/github/basdxz/rightproperguiscale/mixin/mixins/client/minecraft/GuiNewChatMixin.java
@@ -1,5 +1,6 @@
 package com.github.basdxz.rightproperguiscale.mixin.mixins.client.minecraft;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiNewChat;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.util.IChatComponent;
@@ -10,51 +11,116 @@ import org.spongepowered.asm.mixin.injection.callback.*;
 
 import static com.github.basdxz.rightproperguiscale.util.Util.toIScaledResolutionMixin;
 
+/**
+ * Fixes {@link GuiNewChat} having incorrect mouse position calculation, making it annoying to click on links in chat.
+ */
 @Unique
 @Mixin(GuiNewChat.class)
 public abstract class GuiNewChatMixin {
+    @Shadow
+    @Final
+    private Minecraft mc;
+    /**
+     * X position mouse offset constant from: {@link GuiNewChat#func_146236_a(int, int)}
+     */
     private static final int X_OFFSET = -3;
+    /**
+     * X position mouse offset constant from: {@link GuiNewChat#func_146236_a(int, int)}
+     */
     private static final int Y_OFFSET = -27;
+    /**
+     * Last captured mouse X position.
+     */
+    private int lastMouseXPos;
+    /**
+     * Last captured mouse Y position.
+     */
+    private int lastMouseYPos;
+    /**
+     * Scale factor from the last captured scaled resolution.
+     */
+    private float lastScaleFactor;
 
-    private int lastMousePosX;
-    private int lastMousePosY;
-    private float lastScaleFactorFloat;
-
+    /**
+     * Injects right after the Resolution Scale has been creates, captures it and all the other variables in scope.
+     *
+     * @param mouseXPos        mouse X position
+     * @param mouseYPos        mouse Y position
+     * @param cir              mixin callback info returnable
+     * @param scaledResolution scaled resolution
+     */
     @Inject(method = "func_146236_a(II)Lnet/minecraft/util/IChatComponent;",
             at = @At(value = "INVOKE",
                      target = "Lnet/minecraft/client/gui/ScaledResolution;getScaleFactor()I"),
             locals = LocalCapture.CAPTURE_FAILHARD,
             require = 1)
-    private void storeVariablesForScaleFix(int mousePosX,
-                                           int mousePosY,
-                                           CallbackInfoReturnable<IChatComponent> cir,
-                                           ScaledResolution scaledResolution) {
-        lastMousePosX = mousePosX;
-        lastMousePosY = mousePosY;
-        lastScaleFactorFloat = toIScaledResolutionMixin(scaledResolution).scaleFactorF();
+    private void captureVariables(int mouseXPos,
+                                  int mouseYPos,
+                                  CallbackInfoReturnable<IChatComponent> cir,
+                                  ScaledResolution scaledResolution) {
+        storeVariables(mouseXPos, mouseYPos, toIScaledResolutionMixin(scaledResolution).scaleFactorF());
     }
 
-    // Minecraft chat scale
-    @Shadow
-    public abstract float func_146244_h();
+    private void storeVariables(int mousePosX, int mousePosY, float lastScaleFactorFloat) {
+        this.lastMouseXPos = mousePosX;
+        this.lastMouseYPos = mousePosY;
+        this.lastScaleFactor = lastScaleFactorFloat;
+    }
 
+    /**
+     * Injects where the relative mouse X position is set and replaces it with a proper value.
+     *
+     * @param relativeMouseXPos relative mouse X position.
+     * @return relative mouse X position
+     */
     @Redirect(method = "func_146236_a(II)Lnet/minecraft/util/IChatComponent;",
               at = @At(value = "INVOKE",
                        target = "Lnet/minecraft/util/MathHelper;floor_float(F)I",
                        ordinal = 0),
               require = 1)
-    private int fixChatComponentXPos(float ignored) {
-        return MathHelper.floor_float(
-                (lastMousePosX / lastScaleFactorFloat + X_OFFSET) / this.func_146244_h());
+    private int fixMouseXPos(float relativeMouseXPos) {
+        return relativeMouseXPos();
     }
 
+    /**
+     * Provides relative mouse X position.
+     *
+     * @return relative mouse X position
+     */
+    private int relativeMouseXPos() {
+        return MathHelper.floor_float((lastMouseXPos / lastScaleFactor + X_OFFSET) / chatScale());
+    }
+
+    /**
+     * Injects where the relative mouse Y position is set and replaces it with a proper value.
+     *
+     * @param relativeMouseYPos relative mouse Y position.
+     * @return relative mouse Y position
+     */
     @Redirect(method = "func_146236_a(II)Lnet/minecraft/util/IChatComponent;",
               at = @At(value = "INVOKE",
                        target = "Lnet/minecraft/util/MathHelper;floor_float(F)I",
                        ordinal = 1),
               require = 1)
-    private int fixChatComponentYPos(float ignored) {
-        return MathHelper.floor_float(
-                (lastMousePosY / lastScaleFactorFloat + Y_OFFSET) / this.func_146244_h());
+    private int fixMouseYPos(float relativeMouseYPos) {
+        return relativeMouseYPos();
+    }
+
+    /**
+     * Provides relative mouse Y position.
+     *
+     * @return relative mouse Y position
+     */
+    private int relativeMouseYPos() {
+        return MathHelper.floor_float((lastMouseYPos / lastScaleFactor + Y_OFFSET) / chatScale());
+    }
+
+    /**
+     * Provides the Minecraft chat scale.
+     *
+     * @return Minecraft chat scale
+     */
+    private float chatScale() {
+        return mc.gameSettings.chatScale;
     }
 }
