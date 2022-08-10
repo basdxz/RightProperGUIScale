d7d50ab737c5fd15820a44e7272eb1594883e0f5
basdxz <tudurap.com@gmail.com>
2022-08-10 23:48:45 +0100

    Added documentation to config loader

--- src/main/java/com/github/basdxz/rightproperguiscale/mixin/mixins/client/minecraft/MinecraftGUIScaleConfigLoading.java
+++ src/main/java/com/github/basdxz/rightproperguiscale/mixin/mixins/client/minecraft/MinecraftGUIScaleConfigLoading.java
@@ -1,13 +1,22 @@
 package com.github.basdxz.rightproperguiscale.mixin.mixins.client.minecraft;
 
+import com.github.basdxz.rightproperguiscale.GUIScale;
 import com.github.basdxz.rightproperguiscale.config.RightProperGUIScaleConfig;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.*;
 
+/**
+ * Mixin to load the {@link RightProperGUIScaleConfig} before {@link GUIScale} to correctly set the default values.
+ */
 @Mixin(Minecraft.class)
 public abstract class MinecraftGUIScaleConfigLoading {
+    /**
+     * Injects right before the game starts loading and loads {@link RightProperGUIScaleConfig} before anything else.
+     *
+     * @param ci mixin callback info
+     */
     @Inject(method = "startGame()V",
             at = @At(value = "HEAD"),
             require = 1)
